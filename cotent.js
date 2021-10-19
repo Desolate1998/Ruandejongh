764cdbe14b08b13ee51e6005159ffe88744b96e3
Ruan de Jongh <ruandejongh2@outlook.com>
2021-10-19 20:45:50 +0200

    ddd

--- cotent.js
+++ cotent.js
@@ -4,31 +4,51 @@ var menu = [
     document.getElementById('protfolio'),
 ]
 var menuId = 0
+var A = document.getElementById('A')
+menu[menuId].append(A);
+
+
+
+function animateMenuSelect() {
+    var menuElement = document.getElementById('menu');
+    var menuContent = document.getElementById('displayContent');
+    menuElement.style.left += '100px'
+    menuElement.style.position = 'absolute'
+}
+
 
 menu[0].addEventListener("click", function(e) {
-    console.log('clicked')
+
     menu[1].classList.remove('focused');
     menu[2].classList.remove('focused');
     menu[0].classList.add('focused');
     menuId = 0
     menuSound.play()
-    console.log('clicked')
+    menu[0].append(A);
+    document.getElementById('displayContent').innerHTML = pages.aboutMe
 })
 
 menu[1].addEventListener("click", function(e) {
+    menu[1].append(A);
     menu[0].classList.remove('focused');
     menu[2].classList.remove('focused');
     menu[1].classList.add('focused');
     menuId = 1
     menuSound.play()
+
+    document.getElementById('displayContent').innerHTML = pages.education
+
 })
 
 menu[2].addEventListener("click", function(e) {
+    menu[2].append(A);
     menu[1].classList.remove('focused');
     menu[0].classList.remove('focused');
     menu[2].classList.add('focused');
     menuId = 2
     menuSound.play()
+    document.getElementById('displayContent').innerHTML = pages.portfolio
+
 })
 
 
@@ -38,14 +58,19 @@ var menuSound = new Audio('./Sounds/Menu Selection Click.wav')
 menuSound.playbackRate = 10
 document.onkeydown = checkKey;
 
+
 function checkKey(e) {
+
+    animateMenuSelect()
     e = e || window.event;
     if (e.keyCode == '38') {
         //up arrow was clicked
         if (menuId != 0) {
             menuSound.play()
+
             menu[menuId].classList.remove('focused');
             menu[--menuId].classList.add('focused');
+            menu[menuId].append(A);
         }
 
     } else if (e.keyCode == '40') {
@@ -54,7 +79,9 @@ function checkKey(e) {
             menuSound.play()
 
             menu[menuId].classList.remove('focused');
+
             menu[++menuId].classList.add('focused');
+            menu[menuId].append(A);
         }
 
     } else if (e.keyCode == '37') {
---
+++ pages.js
@@ -0,0 +1,6 @@
+var pages = {
+    aboutMe: `<h1> About</h1>`,
+    education: `<h1> Education</h1>`,
+    portfolio: `<h1> Portfolio</h1>`
+
+}
